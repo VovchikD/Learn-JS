157cfaae76447c9da8059fc7f414f92f2841364c
Volodia <hryhorskyiv@gmail.com>
2024-11-25 12:04:39 +0200

    add edit with jquery

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,8 +20,9 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: "Post was successfully created." }
+        format.html { redirect_to posts_path, notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
+        format.js
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
